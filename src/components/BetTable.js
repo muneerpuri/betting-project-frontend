22a343382e8dee7eda6cbabea9746efeff190f9f
Muneer Puri <muneer.p@tunicalabsmedia.com>
2022-04-07 14:59:41 +0530

    sort out old dates

--- src/components/BetTable.js
+++ src/components/BetTable.js
@@ -1,65 +1,124 @@
 import React from "react";
-import {  useHistory } from "react-router";
-import { Table,Tabs,Tab, Pagination, Container ,Spinner} from "react-bootstrap";
+import { useHistory } from "react-router";
+import {
+  Table,
+  Tabs,
+  Tab,
+  Pagination,
+  Container,
+  Spinner,
+} from "react-bootstrap";
 function BetTable() {
   const history = useHistory();
   const [activePageTab, setActivePageTab] = React.useState(1);
   const [activePageTabUpcoming, setActivePageTabUpcoming] = React.useState(1);
-  const [content,setContent] = React.useState([])
-  const [loading,setLoading] = React.useState(false)
-  const [upcomingData,setUpcomingData] = React.useState([])
+  const [content, setContent] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [upcomingData, setUpcomingData] = React.useState([]);
   const itemsPerPage = 10;
   let timeZoneNow = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  React.useEffect(()=>{
-    setLoading(true)
-      fetch(process.env.REACT_APP_BACKEND+"/games")
-      .then(res=>res.json())
-      .then(res=>{
-          console.log(res.data)
-          let dateSortedArray = res.data.sort(function(a, b) {
-            var dateA = new Date(a.attributes.gameTime);
-            var dateB = new Date(b.attributes.gameTime);
-            return dateA && a.attributes.status === "UPCOMING" - dateB && b.attributes.status === "PROCESSED" 
-        })
-        let upcomingArr = dateSortedArray.filter((el)=>el.attributes.status === "UPCOMING")
-        let processedArr = dateSortedArray.filter((el)=>el.attributes.status === "PROCESSED")
-        setUpcomingData(upcomingArr)
-        setContent(upcomingArr.concat(processedArr))
-        setLoading(false)})
-      .catch(e=>{setLoading(false)})
+  React.useEffect(() => {
+    setLoading(true);
+    fetch(process.env.REACT_APP_BACKEND + "/games")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.data);
+        let dateSortedArray = res.data.sort(function (a, b) {
+          var dateA = new Date(a.attributes.gameTime);
+          var dateB = new Date(b.attributes.gameTime);
+          return (
+            dateA &&
+            a.attributes.status === "UPCOMING" - dateB &&
+            b.attributes.status === "PROCESSED"
+          );
+        });
+        let upcomingArr = dateSortedArray
+          .filter((e) => {
+            var inputDate = new Date(e.attributes.gameTime);
 
-  },[])
-  const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-  setInterval(()=>{
-    setLoading(true)
-      fetch(process.env.REACT_APP_BACKEND+"/games")
-      .then(res=>res.json())
-      .then(res=>{
-          console.log(res.data)
-          let dateSortedArray = res.data.sort(function(a, b) {
-            var dateA = new Date(a.attributes.gameTime);
-            var dateB = new Date(b.attributes.gameTime);
-            return dateA && a.attributes.status === "UPCOMING" - dateB && b.attributes.status === "PROCESSED" 
-        })
-        let upcomingArr = dateSortedArray.filter((el)=>el.attributes.status === "UPCOMING")
-        let processedArr = dateSortedArray.filter((el)=>el.attributes.status === "PROCESSED")
-        setUpcomingData(upcomingArr)
-        setContent(upcomingArr.concat(processedArr))
-        setLoading(false)})
-      .catch(e=>{setLoading(false)})
-  },180000)
+            var todaysDate = new Date();
+
+            if (inputDate > todaysDate) {
+              return e;
+            }
+          })
+          .filter((el) => el.attributes.status === "UPCOMING");
+        let processedArr = dateSortedArray
+          .filter((e) => {
+            var inputDate = new Date(e.attributes.gameTime);
+
+            var todaysDate = new Date();
+
+            if (inputDate > todaysDate) {
+              return e;
+            }
+          })
+          .filter((el) => el.attributes.status === "PROCESSED");
+        setUpcomingData(upcomingArr);
+        setContent(upcomingArr.concat(processedArr));
+        setLoading(false);
+      })
+      .catch((e) => {
+        setLoading(false);
+      });
+  }, []);
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  setInterval(() => {
+    setLoading(true);
+    fetch(process.env.REACT_APP_BACKEND + "/games")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.data);
+        let dateSortedArray = res.data.sort(function (a, b) {
+          var dateA = new Date(a.attributes.gameTime);
+          var dateB = new Date(b.attributes.gameTime);
+          return (
+            dateA &&
+            a.attributes.status === "UPCOMING" - dateB &&
+            b.attributes.status === "PROCESSED"
+          );
+        });
+        let upcomingArr = dateSortedArray.filter(
+          (el) => el.attributes.status === "UPCOMING"
+        );
+        let processedArr = dateSortedArray.filter(
+          (el) => el.attributes.status === "PROCESSED"
+        );
+        setUpcomingData(upcomingArr);
+        setContent(upcomingArr.concat(processedArr));
+        setLoading(false);
+      })
+      .catch((e) => {
+        setLoading(false);
+      });
+  }, 180000);
   const indexOfLastTodo = activePageTab * itemsPerPage;
-    const indexOfFirstTodo = indexOfLastTodo - itemsPerPage;
-    const activeData = content.slice(indexOfFirstTodo, indexOfLastTodo);
+  const indexOfFirstTodo = indexOfLastTodo - itemsPerPage;
+  const activeData = content.slice(indexOfFirstTodo, indexOfLastTodo);
 
-    const indexOfLastTodoU = activePageTabUpcoming * itemsPerPage;
-    const indexOfFirstTodoU = indexOfLastTodoU - itemsPerPage;
-    const activeDataU = upcomingData.slice(indexOfFirstTodoU, indexOfLastTodoU);
+  const indexOfLastTodoU = activePageTabUpcoming * itemsPerPage;
+  const indexOfFirstTodoU = indexOfLastTodoU - itemsPerPage;
+  const activeDataU = upcomingData.slice(indexOfFirstTodoU, indexOfLastTodoU);
 
   let itemsU = [];
-  for (let number = 1; number <=Math.ceil(upcomingData.length / itemsPerPage); number++) {
+  for (
+    let number = 1;
+    number <= Math.ceil(upcomingData.length / itemsPerPage);
+    number++
+  ) {
     itemsU.push(
       <Pagination.Item
         style={{ backrgound: "black !important" }}
@@ -73,7 +132,11 @@ function BetTable() {
     );
   }
   let items = [];
-  for (let number = 1; number <=Math.ceil(content.length / itemsPerPage); number++) {
+  for (
+    let number = 1;
+    number <= Math.ceil(content.length / itemsPerPage);
+    number++
+  ) {
     items.push(
       <Pagination.Item
         style={{ backrgound: "black !important" }}
@@ -86,133 +149,196 @@ function BetTable() {
       </Pagination.Item>
     );
   }
-  
+
   return (
     <Container fluid bg="dark" className="p-2 pagination-bg-dark bg-dark ">
-     {loading ?<Container className=" mt-5 mb-5 d-flex justify-content-center align-items-center"> <Spinner animation="border" variant="light" /></Container>:
-     
-     <Tabs  defaultActiveKey="home" id="uncontrolled-tab-example" className="mb-3 text-white">
-  <Tab className="text-white" eventKey="home" title="Upcoming">
-  <>
-     {/* <p className="fw-normal text-white">{timeZoneNow}</p> */}
-     <Table className="w-100" responsive striped bordered hover variant="dark">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>GAME</th>
-            <th>DESCRIPTION</th>
-            <th>BET TYPE</th>
-            <th>AMOUNT</th>
-            <th>TIME</th>
-            <th>STATUS</th>
-          </tr>
-        </thead>
-        <tbody>
-         {content.length>0 &&  
-         
-         activeDataU.map((el,index)=>{
-           let date= new Date(el.attributes.gameTime)
-           let data = date.toLocaleString("en-US", {
-            timeZone: timeZoneNow 
-        })
-        let time = data.split(', ')
-        //    console.log(date)
-          
-        //    let intlDateObj = new Intl.DateTimeFormat('en-US', {
-        //     timeZone: timeZoneNow
-        // });
-        // let Time = intlDateObj.format(date);
-           return  <tr onClick={()=>history.push(`/makebet/${el.id}`)} className="pe-auto">
-            <td>{index+1}</td>
-            <td>{el.attributes.game}</td>
-            <td>{el.attributes.description}</td>
-            <td>{el.attributes.betType}</td>
-            <td>{el.attributes.amount}</td>
-            <td> {monthNames[date.getMonth()]+" "+ date.getDate() +" "+ date.getFullYear()+ ", " +time[1]}
-            </td>
-            <td className={el.attributes.status==="UPCOMING"?'text-warning':el.attributes.status==="PROCESSED"?'text-success':el.attributes.status==="LOSS"?'text-info':""}>{el.attributes.status}</td>
-          </tr> 
-
-         })}
-          
-        </tbody>
-      </Table>
-      <Container
-        bg="dark"
-        className="p-2 pagination-bg-dark bg-dark text-secondary d-flex justify-content-center align-items-center"
-      >
-        <Pagination
-          style={{ backrgound: "black !important" }}
-          className="pagination-bg-dark bg-dark"
+      {loading ? (
+        <Container className=" mt-5 mb-5 d-flex justify-content-center align-items-center">
+          {" "}
+          <Spinner animation="border" variant="light" />
+        </Container>
+      ) : (
+        <Tabs
+          defaultActiveKey="home"
+          id="uncontrolled-tab-example"
+          className="mb-3 text-white"
         >
-          {itemsU}
-        </Pagination>
-      </Container>
-      </>
-  </Tab>
-  <Tab className="text-white" eventKey="profile" title="All">
-  <>
-     {/* <p className="fw-normal text-white">{timeZoneNow}</p> */}
-     <Table className="w-100" responsive striped bordered hover variant="dark">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>GAME</th>
-            <th>DESCRIPTION</th>
-            <th>BET TYPE</th>
-            <th>AMOUNT</th>
-            <th>TIME</th>
-            <th>STATUS</th>
-          </tr>
-        </thead>
-        <tbody>
-         {content.length>0 &&  
-         
-         activeData.map((el,index)=>{
-           let date= new Date(el.attributes.gameTime)
-           let data = date.toLocaleString("en-US", {
-            timeZone: timeZoneNow 
-        })
-        let time = data.split(', ')
-        //    console.log(date)
-          
-        //    let intlDateObj = new Intl.DateTimeFormat('en-US', {
-        //     timeZone: timeZoneNow
-        // });
-        // let Time = intlDateObj.format(date);
-           return  <tr onClick={()=>history.push(`/makebet/${el.id}`)} className="pe-auto">
-            <td>{index+1}</td>
-            <td>{el.attributes.game}</td>
-            <td>{el.attributes.description}</td>
-            <td>{el.attributes.betType}</td>
-            <td>{el.attributes.amount}</td>
-            <td> {monthNames[date.getMonth()]+" "+ date.getDate() +" "+ date.getFullYear()+ ", " +time[1]}
-            </td>
-            <td className={el.attributes.status==="UPCOMING"?'text-warning':el.attributes.status==="PROCESSED"?'text-success':el.attributes.status==="LOSS"?'text-info':""}>{el.attributes.status}</td>
-          </tr> 
+          <Tab className="text-white" eventKey="home" title="Upcoming">
+            <>
+              {/* <p className="fw-normal text-white">{timeZoneNow}</p> */}
+              <Table
+                className="w-100"
+                responsive
+                striped
+                bordered
+                hover
+                variant="dark"
+              >
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>GAME</th>
+                    <th>DESCRIPTION</th>
+                    <th>BET TYPE</th>
+                    <th>AMOUNT</th>
+                    <th>TIME</th>
+                    <th>STATUS</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {content.length > 0 &&
+                    activeDataU.map((el, index) => {
+                      let date = new Date(el.attributes.gameTime);
+                      let data = date.toLocaleString("en-US", {
+                        timeZone: timeZoneNow,
+                      });
+                      let time = data.split(", ");
+                      //    console.log(date)
 
-         })}
-          
-        </tbody>
-      </Table>
-      <Container
-        bg="dark"
-        className="p-2 pagination-bg-dark bg-dark text-secondary d-flex justify-content-center align-items-center"
-      >
-        <Pagination
-          style={{ backrgound: "black !important" }}
-          className="pagination-bg-dark bg-dark"
-        >
-          {items}
-        </Pagination>
-      </Container>
-      </>
-  </Tab>
-</Tabs>
-     
-     
-     
-      }
+                      //    let intlDateObj = new Intl.DateTimeFormat('en-US', {
+                      //     timeZone: timeZoneNow
+                      // });
+                      // let Time = intlDateObj.format(date);
+                      return (
+                        <tr
+                          onClick={() => history.push(`/makebet/${el.id}`)}
+                          className="pe-auto"
+                        >
+                          <td>{index + 1}</td>
+                          <td>{el.attributes.game}</td>
+                          <td>{el.attributes.description}</td>
+                          <td>{el.attributes.betType}</td>
+                          <td>{el.attributes.amount}</td>
+                          <td>
+                            {" "}
+                            {monthNames[date.getMonth()] +
+                              " " +
+                              date.getDate() +
+                              " " +
+                              date.getFullYear() +
+                              ", " +
+                              time[1]}
+                          </td>
+                          <td
+                            className={
+                              el.attributes.status === "UPCOMING"
+                                ? "text-warning"
+                                : el.attributes.status === "PROCESSED"
+                                ? "text-success"
+                                : el.attributes.status === "LOSS"
+                                ? "text-info"
+                                : ""
+                            }
+                          >
+                            {el.attributes.status}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </Table>
+              <Container
+                bg="dark"
+                className="p-2 pagination-bg-dark bg-dark text-secondary d-flex justify-content-center align-items-center"
+              >
+                <Pagination
+                  style={{ backrgound: "black !important" }}
+                  className="pagination-bg-dark bg-dark"
+                >
+                  {itemsU}
+                </Pagination>
+              </Container>
+            </>
+          </Tab>
+          <Tab className="text-white" eventKey="profile" title="All">
+            <>
+              {/* <p className="fw-normal text-white">{timeZoneNow}</p> */}
+              <Table
+                className="w-100"
+                responsive
+                striped
+                bordered
+                hover
+                variant="dark"
+              >
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>GAME</th>
+                    <th>DESCRIPTION</th>
+                    <th>BET TYPE</th>
+                    <th>AMOUNT</th>
+                    <th>TIME</th>
+                    <th>STATUS</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {content.length > 0 &&
+                    activeData.map((el, index) => {
+                      let date = new Date(el.attributes.gameTime);
+                      let data = date.toLocaleString("en-US", {
+                        timeZone: timeZoneNow,
+                      });
+                      let time = data.split(", ");
+                      //    console.log(date)
+
+                      //    let intlDateObj = new Intl.DateTimeFormat('en-US', {
+                      //     timeZone: timeZoneNow
+                      // });
+                      // let Time = intlDateObj.format(date);
+                      return (
+                        <tr
+                          onClick={() => history.push(`/makebet/${el.id}`)}
+                          className="pe-auto"
+                        >
+                          <td>{index + 1}</td>
+                          <td>{el.attributes.game}</td>
+                          <td>{el.attributes.description}</td>
+                          <td>{el.attributes.betType}</td>
+                          <td>{el.attributes.amount}</td>
+                          <td>
+                            {" "}
+                            {monthNames[date.getMonth()] +
+                              " " +
+                              date.getDate() +
+                              " " +
+                              date.getFullYear() +
+                              ", " +
+                              time[1]}
+                          </td>
+                          <td
+                            className={
+                              el.attributes.status === "UPCOMING"
+                                ? "text-warning"
+                                : el.attributes.status === "PROCESSED"
+                                ? "text-success"
+                                : el.attributes.status === "LOSS"
+                                ? "text-info"
+                                : ""
+                            }
+                          >
+                            {el.attributes.status}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </Table>
+              <Container
+                bg="dark"
+                className="p-2 pagination-bg-dark bg-dark text-secondary d-flex justify-content-center align-items-center"
+              >
+                <Pagination
+                  style={{ backrgound: "black !important" }}
+                  className="pagination-bg-dark bg-dark"
+                >
+                  {items}
+                </Pagination>
+              </Container>
+            </>
+          </Tab>
+        </Tabs>
+      )}
     </Container>
   );
 }
